0d81a53ee62ec29c4a2cb49d8bd2912e4a9fa42b
valiantljk <valiantljk@gmail.com>
2018-01-25 12:32:34 -0800

    udpate darshan log produce cs

--- darshan_cs_2018/log_produce/case_jan25_2018/Case3/case1.F90
+++ darshan_cs_2018/log_produce/case_jan25_2018/Case3/case1.F90
@@ -12,33 +12,10 @@ PROGRAM case1
   use iso_fortran_env
   IMPLICIT NONE
 
-  INTERFACE
-     INTEGER FUNCTION ctrunc(a) BIND(C,NAME="ctrunc")
-       USE ISO_C_BINDING
-       USE mpi
-       INTEGER(KIND=MPI_OFFSET_KIND), VALUE :: a
-     END FUNCTION ctrunc
-  END INTERFACE
 
   INTEGER(KIND=int64), PARAMETER :: MegaB = 2097152_int64
 
-! 2**20
-!  INTEGER(KIND=int64), PARAMETER :: N = 1048576_int64
-! 2**27
-!  INTEGER(KIND=int64), PARAMETER :: N = 67108864_int64 
-! 2**30
-  INTEGER(KIND=int64), PARAMETER :: N=1073741824_int64
-! 2**31
-!  INTEGER(KIND=int64), PARAMETER :: N=2147483648_int64
-! 2**32
-!  INTEGER(KIND=int64), PARAMETER :: N=4294967296_int64
-!
-! 2**35
-!  INTEGER(KIND=int64), PARAMETER :: 34359738368_int64
-! 2**36	
-!  INTEGER(KIND=int64), PARAMETER :: 68719476736_int64
-! 2**37	
-!  INTEGER(KIND=int64) ,PARAMETER :: 137438953472_int64
+  INTEGER(KIND=int64), PARAMETER :: N = 67108864_int64 
 
 ! Write 4 large datasets 
 
@@ -86,18 +63,6 @@ PROGRAM case1
   proc_cnt = 0
   offset = 0
 
-! WRITE OUR PSEUDO SUPERBLOCK COLLECTIVELY,
-! ONLY PROC 0 WRITES SOMETHING.
-  DO k = 1, sz_superblock
-     superblock(k) = k
-  ENDDO
-  IF(rank.EQ.proc_cnt)THEN
-     CALL MPI_File_write_all(fh, superblock, sz_superblock, MPI_INTEGER, wstatus, ierr)
-  ELSE
-     CALL MPI_File_write_all(fh, superblock, 0, MPI_INTEGER, wstatus, ierr)
-  ENDIF
-  offset = offset + sz_superblock*sizeof(k)
-
 ! WRITE THE RAW DATA AFTER THE SUPERBLOCK, ALL PROCESSES CONTRIBUTE
 ! TO WRITING THE DATA
 
@@ -110,79 +75,15 @@ PROGRAM case1
 
   CALL raw(fh, rank, nprocs, bufsize, offset)
 
-! A) WRITE META-DATA INTER-DISPERSED BETWEEN RAW DATA WRITES, 4 SETS
-
-  DO k = 1, 4
-
-     ! A.1) WRITE META-DATA, WRITE 512 INDIVIDUAL WRITES, EACH INDIVIDUAL 
-     !      WRITE IS WRITTEN BY A SEPARATE PROCESS
-
-
-     DO i = 1, 512 ! write chunks of metadata  
-        
-        proc_cnt = proc_cnt + 1
-        IF(proc_cnt.GT.nprocs-1) proc_cnt = 0
-        
-        bufsize = MegaB/sizeof(k)/512
-        
-        ALLOCATE(buf(1:bufsize))
-        
-        buf(:) = rank
-        
-        
-        CALL MPI_File_set_view(fh, offset, MPI_INTEGER, MPI_INTEGER, "native", MPI_INFO_NULL, ierr)
-        
-        IF(rank.EQ.proc_cnt)THEN
-           CALL MPI_File_write_all(fh, buf, bufsize, MPI_INTEGER, wstatus, ierr)
-        ELSE
-           CALL MPI_File_write_all(fh, buf, 0, MPI_INTEGER, wstatus, ierr)
-        ENDIF
-        
-        offset = offset + bufsize*sizeof(k)
-        
-        DEALLOCATE(buf)
-        
-     ENDDO
-
-     ! A.2) WRITE RAW DATA COLLECTIVELY; ALL PROCESSES CONTRIBUTE TO WRITING A SECTION
-     !      OF THE DATA SET.
-
-     bufsize = N/nprocs
-
-     CALL raw(fh, rank, nprocs, bufsize, offset)
-     
-  ENDDO
 
 ! EXPAND THE FILE
 
-!  expand_fs = sizeof(i)*N + 524288 + sb_sz
   CALL MPI_Barrier(MPI_COMM_WORLD, ierr)
   t = 0.
   t1 = MPI_Wtime()
 
 ! (1) Expand using MPI IO
   CALL MPI_FILE_GET_SIZE(fh, f_sz, ierr)
-
-  ! Expand using POSIX, one process
-  IF( argv .EQ. '2')THEN
-     t3 = MPI_Wtime()
-     CALL MPI_File_close(fh, ierr)
-     t(3) = MPI_Wtime() - t3;
-     IF(rank.EQ.0)THEN
-        t2 = MPI_Wtime()
-        i = ctrunc(f_sz)
-        t(2) = MPI_Wtime() - t2;
-     ENDIF
-  ELSE 
-     t2 = MPI_Wtime()
-     CALL MPI_File_set_size(fh, f_sz, ierr)
-     t(2) = MPI_Wtime() - t2;
-     
-     t3 = MPI_Wtime()
-     CALL MPI_File_close(fh, ierr)
-     t(3) = MPI_Wtime() - t3;
-
-  ENDIF
 
   t(1) = MPI_Wtime() - t1;
 
--- darshan_cs_2018/log_produce/case_jan25_2018/Case3/runit.sh
+++ darshan_cs_2018/log_produce/case_jan25_2018/Case3/runit.sh
@@ -10,21 +10,11 @@ WRKDIR=$SCRATCH/$SLURM_JOB_ID
 mkdir $WRKDIR
 lfs setstripe -c 64 -S 16m $WRKDIR
 cd $WRKDIR
-out="2"
 cmd="case1"
 tsk=8192 #4096 # -N times 24
 cleanup=yes
 cp $SLURM_SUBMIT_DIR/$cmd .
-# find number of MPI tasks per node
-#set TPN=`echo $SLURM_TASKS_PER_NODE | cut -f 1 -d \(`
-# find number of CPU cores per node
-#set PPN=`echo $SLURM_JOB_CPUS_PER_NODE | cut -f 1 -d \(`
-#echo " $tsk $TPN $PPN $SLURM_TASKS_PER_NODE $SLURM_JOB_CPUS_PER_NODE"
-rm -f $SLURM_SUBMIT_DIR/${out}_$tsk
-for i in `seq 1 4`; do
-  srun -n $tsk ./$cmd
-done
-cp timing $SLURM_SUBMIT_DIR/timing_${tsk}_${out}
+srun -n $tsk ./$cmd
 cd $SLURM_SUBMIT_DIR
 if [ -n "$cleanup" ]; then
   rm -r -f $WRKDIR
